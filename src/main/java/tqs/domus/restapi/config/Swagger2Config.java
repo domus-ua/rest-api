33e9f617017e49ef49d25fbe7c69ca64058fe9f3
Vasco Ramos <vascoalramos@outlook.pt>
2020-05-05 21:47:47 +0100

    💡 Init rest api swagger documentation

---
+++ src/main/java/tqs/domus/restapi/config/Swagger2Config.java
@@ -0,0 +1,43 @@
+package tqs.domus.restapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author Vasco Ramos
+ * @date 05/05/20
+ * @time 21
+ */
+
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build().apiInfo(apiEndPointsInfo());
+    }
+
+
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder().title("Domus REST API")
+                .description("Domus endpoints REST API")
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
+                .build();
+    }
+
+}
+
