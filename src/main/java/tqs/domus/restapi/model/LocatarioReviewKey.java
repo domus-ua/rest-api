36437523cd7520c4f400f49ad6f67280f9291882
Joao <j.vasconcelos99@ua.pt>
2020-05-18 17:02:41 +0100

    Small bug fix

--- src/main/java/tqs/domus/restapi/model/LocatarioReviewKey.java
+++ src/main/java/tqs/domus/restapi/model/LocatarioReviewKey.java
@@ -24,34 +24,5 @@ public class LocatarioReviewKey implements Serializable {
 	@Column(name = "locador_id")
 	private Long locadorId;
 
-	/*
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((locadorId == null) ? 0 : locadorId.hashCode());
-		result = prime * result + ((locatarioId == null) ? 0 : locatarioId.hashCode());
-		return result;
-	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		LocatarioReviewKey other = (LocatarioReviewKey) obj;
-		if (locadorId == null) {
-			if (other.locadorId != null)
-				return false;
-		} else if (!locadorId.equals(other.locadorId)) {
-			return false;
-		}
-		if (locatarioId == null) {
-			return other.locatarioId == null;
-		} else return locatarioId.equals(other.locatarioId);
-	}
-	*/
 }
