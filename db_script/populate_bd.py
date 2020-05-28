ba915dcfa67b0de1b9abafb191aa17388ba321fe
jmnmv12 <j.vasconcelos99@gmail.com>
2020-05-28 11:42:34 +0100

    New houses, reviews and rents

--- db_script/populate_bd.py
+++ db_script/populate_bd.py
@@ -67,6 +67,30 @@ def main(base_url):
             "description": "Apenas são permitidas raparigas", "propertyFeatures": "Wi-Fi;Washing machine;Vacuum cleaner;Air conditioning", "photos": house_photos, "locador": {"id": locador_4_id}}
     house_4_id = post(url=f"{base_url}/houses", json=data).json()["id"]
 
+    house_photos = [HOUSE_5_PHOTO_1, HOUSE_5_PHOTO_2]
+    data = {"street": "Campo 24 de Agosto", "city": "Porto", "postalCode": "4300-503", "noRooms": 4, "noBathrooms": 2, "noGarages": 0, "habitableArea": 130, "available": True, "price": 280, "name": "T4 c/ quarto para rapariga",
+            "description": "Quarto extremamente confortável, localizado na Rua Santos Pousada, próximo da Rua Santa Catarina. Nas imediações poderá encontrar todo o tipo de comércio, transportes e serviços. Dista 500 metros estação de Metro do Campo 24 de Agosto e 20 metros de paragem de autocarro dos STCP.",
+            "propertyFeatures": "Wi-Fi;Washing machine;Vacuum cleaner;Warm Water", "photos": house_photos, "locador": {"id": locador_3_id}}
+    house_5_id = post(url=f"{base_url}/houses", json=data).json()["id"]
+
+    house_photos = [HOUSE_6_PHOTO_1, HOUSE_6_PHOTO_2]
+    data = {"street": "Campo 24 de Agosto", "city": "Porto", "postalCode": "4300-503", "noRooms": 3, "noBathrooms": 1, "noGarages": 0, "habitableArea": 120, "available": True, "price": 220, "name": "Quartos no centro do Porto",
+            "description": "Quarto extremamente confortável.Equipado com microondas, placa, forno, cafeteira, frigorífico e torradeira.Não possui armário nos quartos, podendo o arrendatário providenciá-lo e levantar quando da saída.",
+            "propertyFeatures": "Wi-Fi;Washing machine;Vacuum cleaner;Fire extinguisher", "photos": house_photos, "locador": {"id": locador_4_id}}
+    house_6_id = post(url=f"{base_url}/houses", json=data).json()["id"]
+
+    house_photos = [HOUSE_7_PHOTO_1, HOUSE_7_PHOTO_2]
+    data = {"street": "Rua Heliodoro Salgado", "city": "Lisboa", "postalCode": "1170-174", "noRooms": 3, "noBathrooms": 2, "noGarages": 0, "habitableArea": 120, "available": True, "price": 300, "name": "Quartos em T3 Lisboa",
+            "description": "Despesas incluídas. A 10 minutos a pé do metro dos Anjos, encontrará este apartamento totalmente remodelado e que conta com um ambiente internacional bastante descontraído.",
+            "propertyFeatures": "Wi-Fi;Vacuum cleaner;Fire extinguisher", "photos": house_photos, "locador": {"id": locador_2_id}}
+    house_7_id = post(url=f"{base_url}/houses", json=data).json()["id"]
+
+    house_photos = [HOUSE_8_PHOTO_1, HOUSE_8_PHOTO_2]
+    data = {"street": "Rua Heliodoro Salgado", "city": "Lisboa", "postalCode": "1170-174", "noRooms": 4, "noBathrooms": 2, "noGarages": 1, "habitableArea": 150, "available": True, "price": 320, "name": "Quartos em T4 Lisboa",
+            "description": "Desejavelmente estudantes ou quadros superiores.Um mês de renda e um mês de caução.Não aceitamos animaisNão fumadoresPrivilegiamos contratos de média ou longa duração.",
+            "propertyFeatures": "Wi-Fi;Vacuum cleaner;Washing machine;Balcony", "photos": house_photos, "locador": {"id": locador_2_id}}
+    house_8_id = post(url=f"{base_url}/houses", json=data).json()["id"]
+
     #######################################
     #       ADD HOUSES TO WISHLIST        #
     #######################################
@@ -93,15 +117,77 @@ def main(base_url):
             "locadorId": locador_3_id, "startDate": "2019-01-01", "endDate": "2020-01-01", "price": 195}
     print(post(url=f"{base_url}/locadores/rent", json=data))
 
+    data = {"locatarioEmail": "locatario3@mail.com", "houseId": house_8_id,
+            "locadorId": locador_2_id, "startDate": "2018-01-01", "endDate": "2018-12-01", "price": 320}
+    print(post(url=f"{base_url}/locadores/rent", json=data))
+
+    data = {"locatarioEmail": "locatario3@mail.com", "houseId": house_7_id,
+            "locadorId": locador_2_id, "startDate": "2017-01-01", "endDate": "2017-10-01", "price": 300}
+    print(post(url=f"{base_url}/locadores/rent", json=data))
+
+    data = {"locatarioEmail": "locatario3@mail.com", "houseId": house_5_id,
+            "locadorId": locador_3_id, "startDate": "2016-01-01", "endDate": "2016-12-01", "price": 280}
+    print(post(url=f"{base_url}/locadores/rent", json=data))
+
     data = {"locatarioEmail": "locatario2@mail.com", "houseId": house_1_id,
             "locadorId": locador_1_id, "startDate": "2018-06-01", "endDate": "2018-12-01", "price": 250}
     print(post(url=f"{base_url}/locadores/rent", json=data))
+
+    data = {"locatarioEmail": "locatario2@mail.com", "houseId": house_2_id,
+            "locadorId": locador_1_id, "startDate": "2017-06-01", "endDate": "2017-12-01", "price": 375}
+    print(post(url=f"{base_url}/locadores/rent", json=data))
+
+    data = {"locatarioEmail": "locatario2@mail.com", "houseId": house_7_id,
+            "locadorId": locador_2_id, "startDate": "2019-06-01", "endDate": "2019-12-01", "price": 300}
+    print(post(url=f"{base_url}/locadores/rent", json=data))
+
+    data = {"locatarioEmail": "locatario2@mail.com", "houseId": house_8_id,
+            "locadorId": locador_2_id, "startDate": "2020-01-01", "endDate": "2020-12-01", "price": 320}
+    print(post(url=f"{base_url}/locadores/rent", json=data))
+
+    data = {"locatarioEmail": "locatario1@mail.com", "houseId": house_2_id,
+            "locadorId": locador_1_id, "startDate": "2018-08-01", "endDate": "2018-12-22", "price": 375}
+    print(post(url=f"{base_url}/locadores/rent", json=data))
+
+    data = {"locatarioEmail": "locatario1@mail.com", "houseId": house_5_id,
+            "locadorId": locador_3_id, "startDate": "2019-03-01", "endDate": "2019-12-01", "price": 280}
+    print(post(url=f"{base_url}/locadores/rent", json=data))
+
+    data = {"locatarioEmail": "locatario1@mail.com", "houseId": house_6_id,
+            "locadorId": locador_4_id, "startDate": "2020-01-01", "endDate": "2020-12-01", "price": 220}
+    print(post(url=f"{base_url}/locadores/rent", json=data))
+
+    
 
     #######################################
     #           CREATE REVIEWS            #
     #######################################
     data = {"houseId": house_2_id, "locatarioId": locatario_2_id,
             "comment": "Very nice!", "rating": 3.5}
+    post(url=f"{base_url}/houses/reviews", json=data)
+
+    data = {"houseId": house_2_id, "locatarioId": locatario_1_id,
+            "comment": "I had an amazing stay!", "rating": 4.5}
+    post(url=f"{base_url}/houses/reviews", json=data)
+
+    data = {"houseId": house_5_id, "locatarioId": locatario_1_id,
+            "comment": "The house is nice, but the street is very noisy!", "rating": 3.5}
+    post(url=f"{base_url}/houses/reviews", json=data)
+
+    data = {"houseId": house_8_id, "locatarioId": locatario_2_id,
+            "comment": "Excellent house with an amazing owner.", "rating": 5}
+    post(url=f"{base_url}/houses/reviews", json=data)
+
+    data = {"houseId": house_7_id, "locatarioId": locatario_2_id,
+            "comment": "The bathroom was always dirty and the windows didn't close.", "rating": 2.5}
+    post(url=f"{base_url}/houses/reviews", json=data)
+
+    data = {"houseId": house_3_id, "locatarioId": locatario_3_id,
+            "comment": "The house is nice, but some walls need to be repainted!", "rating": 4}
+    post(url=f"{base_url}/houses/reviews", json=data)
+
+    data = {"houseId": house_8_id, "locatarioId": locatario_3_id,
+            "comment": "The bedroom was nice and the location of the house is very good!", "rating": 4.5}
     post(url=f"{base_url}/houses/reviews", json=data)
 
 
